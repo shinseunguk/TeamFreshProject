8fe0cf0e51c1290e06110d3841578a12eb67dc59
Wetax_iOS <wetaxmobile@gmail.com>
2022-08-11 17:50:08 +0900

    [신승욱] 과제 작업중

--- TeamFreshProject/ViewController.swift
+++ TeamFreshProject/ViewController.swift
@@ -5,6 +5,8 @@
 //  Created by mac on 2022/08/11.
 //
 
+// 08.11 commit => notion rgb추출하여 placeholder 적용, imageview setImage
+
 import UIKit
 import Alamofire
 
@@ -18,7 +20,23 @@ class ViewController: UIViewController, UITextFieldDelegate {
         // Do any additional setup after loading the view.
         print(#function)
         
+        setUP()
+        
         loginBtn.tag = 0
+    }
+    
+    func setUP(){
+        //아이디 텍스트필드 설정
+        idTextField.attributedPlaceholder = NSAttributedString(string: "아이디", attributes: [NSAttributedString.Key.foregroundColor : UIColor.init(displayP3Red: 144/255, green: 144/255, blue: 149/255, alpha: 1)])
+        idTextField.autocorrectionType = .no
+        idTextField.spellCheckingType = .no
+        idTextField.autocapitalizationType = .none
+        idTextField.returnKeyType = .next
+//        idTextField.textContentType
+        
+        //비밀번호 텍스트필드 설정
+        pwTextField.attributedPlaceholder = NSAttributedString(string: "비밀번호", attributes: [NSAttributedString.Key.foregroundColor : UIColor.init(displayP3Red: 144/255, green: 144/255, blue: 149/255, alpha: 1)])
+        pwTextField.autocorrectionType = .no
     }
     
     override func viewDidLayoutSubviews() {
