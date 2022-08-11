dc3c6172e7081260ed1c3d84e6cff99055f0d561
SeungUkShin <krdut1@gmail.com>
2022-08-11 23:32:17 +0900

    [신승욱] 과제 진행중

--- TeamFreshProject/ViewController.swift
+++ TeamFreshProject/ViewController.swift
@@ -28,15 +28,20 @@ class ViewController: UIViewController, UITextFieldDelegate {
     func setUP(){
         //아이디 텍스트필드 설정
         idTextField.attributedPlaceholder = NSAttributedString(string: "아이디", attributes: [NSAttributedString.Key.foregroundColor : UIColor.init(displayP3Red: 144/255, green: 144/255, blue: 149/255, alpha: 1)])
-        idTextField.autocorrectionType = .no
-        idTextField.spellCheckingType = .no
-        idTextField.autocapitalizationType = .none
-        idTextField.returnKeyType = .next
+        idTextField.spellCheckingType = .no // 자동 맞춤법 사용x
+        idTextField.autocapitalizationType = .none // 자동 첫알파벳 대문자로 변경 사용x
+        idTextField.returnKeyType = .next // 키보드 return 키 타입을 next로 변경 (시뮬레이션 혹은 블루투스 키보드에서는 확인 불가)
+        idTextField.textContentType = .emailAddress // 키보드 ContentType(이메일을 입력 받아야 하기 때문에 이메일로 처리)
 //        idTextField.textContentType
+        idTextField.addLeftPadding()
         
         //비밀번호 텍스트필드 설정
         pwTextField.attributedPlaceholder = NSAttributedString(string: "비밀번호", attributes: [NSAttributedString.Key.foregroundColor : UIColor.init(displayP3Red: 144/255, green: 144/255, blue: 149/255, alpha: 1)])
-        pwTextField.autocorrectionType = .no
+        pwTextField.addLeftPadding()
+        idTextField.spellCheckingType = .no // 자동 맞춤법 사용x
+        idTextField.autocapitalizationType = .none // 자동 첫알파벳 대문자로 변경 사용x
+        idTextField.returnKeyType = .done // 키보드 return 키 타입을 done로 변경 (시뮬레이션 혹은 블루투스 키보드에서는 확인 불가, done터치시 btnAction함수 실행)
+        pwTextField.textContentType = .password // 키보드 ContentType(비밀번호를 입력 받아야 하기 때문에 password로 처리)
     }
     
     override func viewDidLayoutSubviews() {
@@ -50,11 +55,13 @@ class ViewController: UIViewController, UITextFieldDelegate {
         pwTextField.backgroundColor = .systemGray6
         
         //로그인 버튼 #ff6348
-        loginBtn.layer.cornerRadius = 20
+        loginBtn.layer.cornerRadius = 30
     }
 
     @IBAction func btnAction(_ sender: Any) {
         print((sender as AnyObject).tag!)
+        print("idTextField => \(idTextField.text!)")
+        print("pwTextField => \(pwTextField.text!)")
     }
     
 
