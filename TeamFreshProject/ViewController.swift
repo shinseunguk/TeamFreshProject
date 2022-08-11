87e383b554cf72391bdb3e46b8d933f2696268fa
Wetax_iOS <wetaxmobile@gmail.com>
2022-08-11 16:30:38 +0900

    Initial Commit

---
+++ TeamFreshProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TeamFreshProject
+//
+//  Created by mac on 2022/08/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
