2f5b0308df98d99d306bc8bab94463776219c510
Wetax_iOS <wetaxmobile@gmail.com>
2022-08-11 17:41:27 +0900

    [신승욱] 과제 작업중

--- TeamFreshProject/ViewController.swift
+++ TeamFreshProject/ViewController.swift
@@ -6,15 +6,39 @@
 //
 
 import UIKit
-import Alamofi
+import Alamofire
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var pwTextField: UITextField!
+    @IBOutlet weak var loginBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(#function)
+        
+        loginBtn.tag = 0
+    }
+    
+    override func viewDidLayoutSubviews() {
+        
+        //아이디 텍스트필드
+        idTextField.layer.cornerRadius = 10
+        idTextField.backgroundColor = .systemGray6
+        
+        //패스워드 텍스트필드
+        pwTextField.layer.cornerRadius = 10
+        pwTextField.backgroundColor = .systemGray6
+        
+        //로그인 버튼 #ff6348
+        loginBtn.layer.cornerRadius = 20
     }
 
+    @IBAction func btnAction(_ sender: Any) {
+        print((sender as AnyObject).tag!)
+    }
+    
 
 }
 
