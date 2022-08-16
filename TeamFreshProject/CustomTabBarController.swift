9d822ebfe5bdf13a4ed6912798b09f0a98463245
Wetax_iOS <wetaxmobile@gmail.com>
2022-08-16 17:54:54 +0900

    [신승욱] navigtaionBar shadow 적용

--- TeamFreshProject/CustomTabBarController.swift
+++ TeamFreshProject/CustomTabBarController.swift
@@ -11,22 +11,28 @@ import UIKit
 class CustomTabBarController: UITabBarController, UITabBarControllerDelegate {
     
     let appearance = UINavigationBarAppearance()
+    let appearanceTabbar = UITabBarAppearance()
     @IBOutlet weak var uiTabBar: UITabBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("\(#file) \(#function)")
+        //로그인 완료후 네비게이터가 게시판으로 가게끔 구현
+        selectedIndex = 3
         
-        navigationBarSetUp()
+        navigationBarSetUp() // 네비게이션바 setUP
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        print("customtabbar viewWillAppear")
+        print(#function)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        print(#function)
         
         appearance.configureWithOpaqueBackground()
-        appearance.backgroundColor = .white // 네비게이션바 become black 삭제
-        appearance.shadowColor = .white // 네비게이션바 bottom 테두리 삭제
+//        appearance.backgroundColor = .white // 네비게이션바 become black 삭제
+//        appearance.shadowColor = .white // 네비게이션바 bottom 테두리 삭제
         
         self.navigationController?.isNavigationBarHidden = false
         self.navigationController?.navigationBar.tintColor = .systemBlue
@@ -38,22 +44,25 @@ class CustomTabBarController: UITabBarController, UITabBarControllerDelegate {
         self.navigationController?.navigationBar.shadowImage = UIImage()
         self.navigationController?.navigationBar.isTranslucent = false
         
+        //navigationBar Shadow
+        self.navigationController?.navigationBar.layer.shadowOpacity = 0.2
+        self.navigationController?.navigationBar.layer.shadowOffset = CGSize.zero
+        self.navigationController?.navigationBar.layer.shadowRadius = 5
+        
         //네비게이션바 뒤로가기 삭제
         self.navigationItem.hidesBackButton = true
-//
-//        self.navigationController?.navigationBar.topItem?.title = "기프티콘 저장"
         
-//        if #available(iOS 15.0, *) {
-            let appearanceTabbar = UITabBarAppearance()
-            appearanceTabbar.configureWithOpaqueBackground()
-            appearanceTabbar.backgroundColor = UIColor.init(displayP3Red: 242/255, green: 242/255, blue: 247/255, alpha: 1)
-            uiTabBar.standardAppearance = appearanceTabbar
-//            tabBar.scrollEdgeAppearance = tabBar.standardAppearance
-//        }
+        appearanceTabbar.configureWithOpaqueBackground()
+        appearanceTabbar.backgroundColor = UIColor.init(displayP3Red: 242/255, green: 242/255, blue: 247/255, alpha: 1)
+        uiTabBar.standardAppearance = appearanceTabbar
+        
         uiTabBar.layer.borderWidth = 0.30
         uiTabBar.layer.borderColor = UIColor.clear.cgColor
         uiTabBar.clipsToBounds = true
+//        uiTabBar.backgroundColor = .blue // 작동함
         
+        // 네비게이터 tintColor #936f68
+//        uiTabBar.tintColor = UIColor.init(displayP3Red: 147/255, green: 111/255, blue: 104/255, alpha: 1)
         
     }
     
